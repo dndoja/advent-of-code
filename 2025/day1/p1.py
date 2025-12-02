d526681f3253faf6a4296685cf0d59e0755593c9
dndoja <ndoja.damian@gmail.com>
2025-12-02 04:10:56 +0100

    2025: solve day 1

---
+++ 2025/day1/p1.py
@@ -0,0 +1,15 @@
+rots = []
+
+with open("day1/p1.txt") as file:
+    for line in file:
+        rots.append((1 if line[0:1] == "R" else -1, int(line[1:])))
+
+curr = 50
+result = 0
+
+for rot_coeff, rot_amount in rots:
+    curr = (curr + rot_coeff * rot_amount) % 100
+    if curr == 0:
+        result += 1
+
+print(result)
---
+++ 2025/day1/p2.py
@@ -0,0 +1,15 @@
+rots = []
+
+with open("day1/p1.txt") as file:
+    for line in file:
+        rots.append((1 if line[0:1] == "R" else -1, int(line[1:])))
+
+curr = 50
+result = 0
+
+for rot_coeff, rot_amount in rots:
+    next = curr + rot_coeff * (rot_amount % 100)
+    result += abs(int(rot_amount / 100)) + (1 if curr != 0 and next <= 0 or next >= 100 else 0)
+    curr = next % 100
+
+print(result)
