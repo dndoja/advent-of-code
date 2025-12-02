f893c609cdf973fead4b5ef6c2aaffa006193a0f
dndoja <ndoja.damian@gmail.com>
2025-12-02 07:10:52 +0100

    2025: solve day 2

---
+++ 2025/day2/p1.py
@@ -0,0 +1,21 @@
+import math
+
+seqs = []
+with open("day2/input.txt") as file:
+    for pair in file.read().split(","):
+        split = pair.split("-")
+        seqs.append((int(split[0]), int(split[1])))
+
+result = 0
+
+for (start, finish) in seqs:
+    for num in range(start,finish+1):
+        digits = math.ceil(math.log10(num))
+        if (digits > 0 and digits % 2 == 0): 
+            mul = int(math.pow(10, digits / 2))
+            right = int(num % mul) * mul
+            left = num - int(num % mul)
+            if (right == left):
+                result += num
+
+print(result)
---
+++ 2025/day2/p2.py
@@ -0,0 +1,37 @@
+import math
+
+seqs = []
+with open("day2/input.txt") as file:
+    for pair in file.read().split(","):
+        split = pair.split("-")
+        seqs.append((int(split[0]), int(split[1])))
+
+
+def is_repeating(num, digits, parts):
+    if digits % parts != 0:
+        return False
+
+    mul = int(math.pow(10, digits / parts))
+    prev = None
+
+    for i in range(0, parts):
+        part = int(num // math.pow(mul, i) % mul)
+        if prev is not None and part != prev:
+            return False
+        else:
+            prev = part
+
+    return prev != 0
+
+result = 0
+
+for start, finish in seqs:
+    for num in range(start, finish + 1):
+        digits = math.ceil(math.log10(num))
+        if digits > 1:
+            for i in range(2, digits + 1):
+                if is_repeating(num, digits, i):
+                    result += num
+                    break
+
+print(result)
