8aea0df6a3f05cf57533ed661331223f4adc15e5
dndoja <ndoja.damian@gmail.com>
2025-12-06 07:27:22 +0100

    2025: solve day 6

---
+++ 2025/day6p1.py
@@ -0,0 +1,30 @@
+result = 0
+operands = []
+operators = []
+
+with open("day6.txt") as file:
+    for line in file:
+        chars = list(line.strip().split(" "))
+        col = 0
+        for char in chars:
+            if char.isdecimal():
+                if col >= len(operands):
+                    operands.append([])
+                print(col, len(operands))
+                operands[col].append(int(char))
+                col += 1
+            elif len(char) > 0:
+                operators.append(char)
+
+for i in range(len(operators)):
+    def op(a, b):
+        return a + b if operators[i] == "+" else a * b
+
+    acc = operands[i][0]
+    for k in range(1, len(operands[i])):
+        acc = op(acc, operands[i][k])
+
+    result += acc
+
+
+print(result)
---
+++ 2025/day6p2.py
@@ -0,0 +1,47 @@
+result = 0
+operands = []
+operators = []
+lines = []
+
+with open("day6.txt") as file:
+    for line in file:
+        lines.append(list(line))
+
+height = len(lines)
+width = len(lines[0])
+expr_index = 0
+operand_index = 0
+
+for x in range(width):
+    found_number = False
+    for y in range(height):
+        ch = lines[y][x]
+        if ch.isdecimal():
+            if expr_index >= len(operands):
+                operands.append([])
+            if operand_index >= len(operands[expr_index]):
+                operands[expr_index].append("")
+            operands[expr_index][operand_index] += ch
+            found_number = True
+        elif ch == "*" or ch == "+":
+            operators.append(ch)
+
+    if found_number:
+        operand_index += 1
+    else:
+        expr_index += 1
+        operand_index = 0
+
+
+for i in range(len(operators)):
+
+    def op(a, b):
+        return a + b if operators[i] == "+" else a * b
+
+    acc = int(operands[i][0])
+    for k in range(1, len(operands[i])):
+        acc = op(acc, int(operands[i][k]))
+
+    result += acc
+
+print(result)
